2ce72d764dfbbc3da4dba2f5f9e1d30ba72fce4c
Lucas Batista da Silva <lucasbsrs@gmail.com>
2019-02-10 13:56:49 -0200

    Criação do Projeto

---
+++ src/main/java/com/lucassilva/ilegrachallenge/IlegrachallengeApplication.java
@@ -0,0 +1,14 @@
+package com.lucassilva.ilegrachallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlegrachallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IlegrachallengeApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/lucassilva/ilegrachallenge/ConversorTeste.java
@@ -0,0 +1,17 @@
+package com.lucassilva.ilegrachallenge;
+
+import org.junit.Test;
+
+public class ConversorTeste {
+
+	@Test
+	public void splitLine() {
+		
+	}
+	
+	@Test
+	public void deveConverterVendedor() {
+		
+	}
+	
+}
